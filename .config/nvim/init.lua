f7463756ce3c870e09c456f358f45104a68da2f6
punycode <zh@punyco.de>
2025-02-14 15:04:19 +0100

    NeoVim: Add minimal setup for https://lazy.folke.io and colorschemes
    The colorscheme plugins are the minimal example, so lazy has anything to
    load initially and is the most lightweight plugin we actually want to
    use later on.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,4 +4,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+require('config.lazy')
+
 -- vim: ts=2 sts=2 sw=2 et
---
+++ .config/nvim/lua/config/plugins/colorschemes.lua
@@ -0,0 +1,14 @@
+-- Colorschemes to load.
+return {
+  { 'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
