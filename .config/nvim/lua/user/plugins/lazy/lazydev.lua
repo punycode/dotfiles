39208837874b735f30dbb04c6337898f62ffa934
punycode <zh@punyco.de>
2025-03-31 17:37:53 +0200

    NeoVim: Enhance Lua LSP support for NeoVim configs using lazydev

---
+++ .config/nvim/lua/user/plugins/lazy/lazydev.lua
@@ -0,0 +1,16 @@
+-- Additional LSP support for Neovim Lua configuration
+return {
+  {
+    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+    -- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
+    },
+    dependencies = { { 'Bilal2453/luvit-meta', lazy = true } },
+  },
+}
