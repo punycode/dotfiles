3f41f1191d4eb56342552a0e13653765cbfd4ddf
punycode <zh@punyco.de>
2025-06-22 15:22:14 +0200

    NeoVim: set recommended style settings for markdown files
    Our default shiftwidth and such are not compatible with the
    recommendations done by markdown linting tools.

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -3,3 +3,9 @@ local set = vim.opt_local
 -- Markdown should be kept to 80 characters textwidth and should not exceed 100
 set.textwidth = 100
 set.colorcolumn = '+0,+20'
+
+-- Recommended style
+set.expandtab = true
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
