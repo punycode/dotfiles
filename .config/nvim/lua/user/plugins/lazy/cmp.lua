dd1b928558dd405c68126e05050f2f056e12be6b
punycode <zh@punyco.de>
2025-10-27 11:08:35 +0100

    NeoVim: Prepare nvim-cmp setup for usage with Neovim 0.11 LSP config
    In preparation of switching the LSP setup code to use Neovim native
    configuration instead of the old nvim-lspconfig, we start by adding
    the completion extension directly to the default LSP config.

--- .config/nvim/lua/user/plugins/lazy/cmp.lua
+++ .config/nvim/lua/user/plugins/lazy/cmp.lua
@@ -7,13 +7,26 @@ return {
   {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
+
     dependencies = {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
       'saadparwaiz1/cmp_luasnip',
     },
+
     config = function()
       require('user.plugins.cmp')
+    end,
+
+    -- This has to be run before entering insert mode, since these capabilities
+    -- need to be registered with LSPs, which are initialized long before
+    -- entering Insert mode.
+    init = function()
+      -- Add the completion capabilities to the default config for every LSP
+      local cmp_nvim_lsp = require('cmp_nvim_lsp')
+      vim.lsp.config('*', {
+        capabilities = cmp_nvim_lsp.default_capabilities(),
+      })
     end,
   },
 }
