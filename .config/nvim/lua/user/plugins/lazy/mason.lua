caa28efffeed3a05d8a83537aa905cff736688f3
punycode <zh@punyco.de>
2025-11-08 15:57:55 +0100

    NeoVim: Add some default non-LSP tools via Mason
    Those are usable in many circumstances and should be installed. Some are
    also configured for default formatting via conform.nvim.

--- .config/nvim/lua/user/plugins/lazy/mason.lua
+++ .config/nvim/lua/user/plugins/lazy/mason.lua
@@ -1,5 +1,26 @@
 -- Setup Mason to install tools (formatters, LSP, etc.)
 return {
-  { 'williamboman/mason.nvim', opts = {} },
-  { 'WhoIsSethDaniel/mason-tool-installer.nvim', lazy = true },
+  {
+    'williamboman/mason.nvim',
+    dependencies = {
+      -- Automatic installation of certain non-LSP tools
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+    },
+    config = function()
+      -- Setup Mason with defaults
+      require('mason').setup({})
+
+      -- Install some often used packages for Mason using the
+      -- tool installer. LSPs are installed/configured indepently
+      -- with nvim-lspconfig/mason-lspconfig
+      require('mason-tool-installer').setup({
+        ensure_installed = {
+          { 'markdownlint' },
+          { 'prettier' },
+          { 'stylua' },
+          { 'yamllint' },
+        },
+      })
+    end,
+  },
 }
