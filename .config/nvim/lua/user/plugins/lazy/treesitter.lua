437e0579621f20e490aec8c3eefc9cffcbc5c203
punycode <zh@punyco.de>
2026-04-20 18:25:46 +0200

    NeoVim: Update nvim-treesitter to new rewrite on main branch

--- .config/nvim/lua/user/plugins/lazy/treesitter.lua
+++ .config/nvim/lua/user/plugins/lazy/treesitter.lua
@@ -2,17 +2,36 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  opts = {
-    auto_install = true,
-    highlight = {
-      enable = true,
+  branch = 'main',
+  lazy = false,
 
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby', 'yaml' } },
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter-textobjects', branch = 'main' },
   },
+
+  config = function()
+    require('nvim-treesitter').install({
+      'bash',
+      'c',
+      'comment',
+      'cpp',
+      'dockerfile',
+      'editorconfig',
+      'git_config',
+      'git_rebase',
+      'gitcommit',
+      'gitignore',
+      'json',
+      'json5',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'python',
+      'regex',
+      'toml',
+      'vim',
+      'yaml',
+    })
+  end,
 }
